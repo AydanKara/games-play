feca9e8571d7a27b0bb0085562090a2b77f3d11a
AydanKara <113279073+AydanKara@users.noreply.github.com>
2024-07-07 05:41:34 +0200

    Add Components Folder
    Add GameList Component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
-import Header from "./Header/Header";
-import Home from "./Home/Home";
+import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
+import GameList from "./components/game-list/GameList";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
 
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/games" element={<GameList />} />
       </Routes>
     </div>
   );
