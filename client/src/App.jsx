99c96a3eaccb85662a01b5239670688ee8be42e0
AydanKara <113279073+AydanKara@users.noreply.github.com>
2024-07-07 05:29:57 +0200

    Install react-router-dom
    Add first route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,17 @@
-import Header from "./Header/Header"
-import Home from "./Home/Home"
+import { Route, Routes } from "react-router-dom";
+import Header from "./Header/Header";
+import Home from "./Home/Home";
 
 function App() {
-
   return (
     <div id="box">
       <Header />
-      <Home />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
